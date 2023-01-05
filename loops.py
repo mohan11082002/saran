203a4fc58686b0196f6e50cadf574b37b252e892
mohan11082002 <116250312+mohan11082002@users.noreply.github.com>
2023-01-05 21:40:32 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,40 @@
+#for loop
+
+
+#r="str"
+#for h in r:
+  #  print(h)
+
+#------------------------------------------    
+
+#for t in range(2,21,6):
+   #  print(t)
+
+    
+#------------------------------------------
+
+#for e in range (21):
+ #   print(e)
+#else:
+ #   print("finished")   
+
+#-----------------------------------------
+
+
+#que="union","helth","tree"
+#ui="1","2","3"
+#for x in que:
+ #   print(x)
+  #  for y in ui:
+   #     print(y)
+  # pass
+#---------------------------------------------
+
+#while loop
+r=1
+while r>100:
+    print(r)
+    r+=2
+
+    
+
